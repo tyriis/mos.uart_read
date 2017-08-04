eeb6954038c1db78ac5ffc5ba38beb544c6d74c1
Nils Mueller <nils@mueller.name>
2017-08-05 01:10:28 +0200

    Change: modified code, make it work :D

--- init.js
+++ init.js
@@ -1,107 +1,88 @@
-load('api_config.js');
+
+ad('api_config.js');
 load('api_gpio.js');
 load('api_mqtt.js');
 load('api_net.js');
 load('api_sys.js');
 load('api_timer.js');
+load('api_http.js');
+load('api_log.js');
+load("api_uart.js");
 
-// Helper C function get_led_gpio_pin() in src/main.c returns built-in LED GPIO
-// ffi() returns a callbale object for the specified C function.
-// As parsing the signature has non-trivial overhead, it's a good practice to
-// store the value for later reuse.
-let get_led_gpio_pin = ffi('int get_led_gpio_pin()');
-// Now call the function to obtain the LED pin number.
-let led = get_led_gpio_pin();
-
-// When C function is invoked only once, it's possible to use this shorthand.
-// let button = ffi('int get_button_gpio_pin()')();
-
-//print("LED GPIO: " + JSON.stringify(led) + "; button GPIO: " + JSON.stringify(button));
+let once = true;
+let topic = 'mos/test';
+let uartNo = 0;
 
 let getInfo = function() {
-  return JSON.stringify({total_ram: Sys.total_ram(), free_ram: Sys.free_ram()});
+  return JSON.stringify({total_ram: Sys.total_ram(), free_ram: Sys.free_ram(), id: Cfg.get('device.id'), uptime: Sys.uptime()});
 };
 
-// Blink built-in LED every second
-//
-
-GPIO.set_mode(led, GPIO.MODE_OUTPUT);
-Timer.set(1000 /* 1 sec */, true /* repeat */, function() {
-  let value = GPIO.toggle(led);
-//  print(value ? 'Tick' : 'Tock', 'uptime:', Sys.uptime(), getInfo());
-}, null);
-
-// Publish to MQTT topic on a button press. Button is wired to GPIO pin 0
-//GPIO.set_button_handler(button, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 200, function() {
-//  let topic = '/devices/' + Cfg.get('device.id') + '/events';
-//  let message = getInfo();
-//  let ok = MQTT.pub(topic, message, 1);
-//  print('Published:', ok ? 'yes' : 'no', 'topic:', topic, 'message:', message);
-//}, null);
+let getTime = function() {
+  HTTP.query({
+    url: 'http://silver:8081',
+    success: function(body, full_http_msg) {
+      let obj = JSON.parse(body);
+      MQTT.pub(topic, JSON.stringify(obj));
+      MQTT.pub(topic, body);
+    },
+    error: function(err) { print(err); },  // Optional
+  });
+};
 
 // Monitor network connectivity.
 Net.setStatusEventHandler(function(ev, arg) {
   let evs = "???";
   if (ev === Net.STATUS_DISCONNECTED) {
     evs = "DISCONNECTED";
+    once = true;
   } else if (ev === Net.STATUS_CONNECTING) {
     evs = "CONNECTING";
   } else if (ev === Net.STATUS_CONNECTED) {
     evs = "CONNECTED";
   } else if (ev === Net.STATUS_GOT_IP) {
     evs = "GOT_IP";
+    getTime();
   }
   print("== Net event:", ev, evs);
 }, null);
 
-let topic = 'mos/test';
-//Timer.set(10000 /* 1 sec */, true /* repeat */, function() {
-//  let res = MQTT.pub(topic, JSON.stringify({ a: 1, b: 2 }), 1);
-  //print('Published:', res ? 'yes' : 'no');
-//}, null);
 
-
-//MQTT.sub(topic, function(conn, topic, msg) {
-//  print('Topic:', topic, 'message:', msg);
-//  let value = GPIO.toggle(led);
-//  Timer.set(10000 /* 1 sec */, false /* repeat */, function() {
-//  let value = GPIO.toggle(led);
-//  print('Topic:', topic, 'message:', msg);
-//  print(value ? 'Tick' : 'Tock', 'uptime:', Sys.uptime(), getInfo());
-//}, null);
-
-//}, null);
-
-load("api_uart.js");
-let uartNo = 0;
-
-let rxAcc = '';
+MQTT.setEventHandler(function(conn, ev, edata) {
+  if (once && ev === 202 /* MQTT CONNACK*/) {
+    once = false;
+    MQTT.pub(topic, getInfo(), 1);
+  }
+}, null);
 
 UART.setConfig(uartNo, {
   baudRate: 115200,
   esp8266: {
-    gpio: {
-      rx: 3,
-      tx: 1,
-    },
+    rx: 3,
+    tx: 1
   },
 });
 
+UART.setRxEnabled(uartNo, true);
 UART.setDispatcher(uartNo, function(uartNo, ud) {
   let ra = UART.readAvail(uartNo);
-
   if (ra > 0) {
     let data = UART.read(uartNo);
-    MQTT.pub(topic, JSON.stringify(data), 1);
-    // print("Received UART data:", data);
-    rxAcc += data;
+    if (data) {
+      MQTT.pub(topic, data.slice(0, -2), 1);
+    }
   }
 }, null);
 
-//UART.setRxEnabled(uartNo, true);
-
-rxAcc = '';
-
-
-
+MQTT.sub(topic, function(conn, topic, msg) {
+  print('Topic:', topic, 'message:', msg);
+  if (msg === 'led_on') {
+    let value = GPIO.toggle(led);
+  } else if (msg[0] === '/') {
+    UART.write(uartNo, msg + "\r\n");
+  } else if (msg === 'time') {
+    print(msg);
+    getTime();
+    Log.info('test');
+  }
+}, null);
 
